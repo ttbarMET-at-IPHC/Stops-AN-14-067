7ce0fe3107a7eefc85ba320584a1ed0d6c1bf050
Administrator <sigamani@cern.ch>
2014-04-09 16:56:48 +0200

    Update

--- runBDT/MakeBDTtree.py
+++ runBDT/MakeBDTtree.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python -tt
 import os
 import sys
 import re
@@ -7,8 +7,7 @@ import time
 
 pwd = os.environ['PWD']
 
-#dataset_name        = ['t2bw_025','t2bw_050', 't2bw_075', 't2tt_all', 'ttbar_1l', 'ttbar_2l', 'wjets_all', 'others_all'] 
-dataset_name        = ['t2bw_025','t2bw_050', 't2bw_075', 't2tt_all']
+dataset_name        = ['t2bw_025','t2bw_050', 't2bw_075', 't2tt_all', 'ttbar_1l', 'ttbar_2l', 'wjets_all', 'others_all'] 
 queue               = '1nh'
 executable          = 'MakeBDTtree'
 deltaMRegion        = ['R1','R2','R3','R4','R5','R6']
@@ -17,49 +16,62 @@ deltaMArguments     = ['100 200 0 800', '200 300 0 800', '300 400 0 800', '400 5
 
 for z in range(len(dataset_name)):
 
-
-    for y in range(len(deltaMRegion)):
-
-
-	inputlist = "../inputFileLists/"+dataset_name[z]+".list"
-	outputdir = pwd+"/BDT3_skim_MT100/"+dataset_name[z]+"/"+deltaMRegion[y]
-	     
-	os.system("rm -r "+outputdir)
-	os.system("mkdir -p "+outputdir)
-	os.system("mkdir -p "+outputdir+"/log/")
-	os.system("mkdir -p "+outputdir+"/input/")
-	os.system("mkdir -p "+outputdir+"/output/")
-	os.system("mkdir -p "+outputdir+"/src/")
+    if (z < 4):
+        range_ = len(deltaMRegion)
+    else:
+        range_ = 1
 
 
-	input = open(inputlist)
-	inputfiles = input.readlines()
-	ijob=0
+    for y in range(range_):
 
-	while (len(inputfiles) > 0):
-	    inputfilename = outputdir+"/input/input_"+str(ijob)
-	    inputfile = open(inputfilename,'w')
-	    for line in range(min(1,len(inputfiles))):
-		ntpfile = inputfiles.pop()
-		if ntpfile != '':
-		    inputfile.write(ntpfile)
+        inputlist = "../inputFileLists/"+dataset_name[z]+".list"
+        outputdir1 = pwd+"/BDT4_skim_MT100/"+dataset_name[z]
 
-	    inputfile.close()
-
-
-            #if (y > 6):
-    	    #    output = dataset_name[z]
-	    #else:
-	    output = dataset_name[z]+"_"+deltaMRegion[y]
+        if (z < 4):
+            outputdir = outputdir1+"/"+deltaMRegion[y]
+        else:	  
+            outputdir = outputdir1
+    
+        
+        os.system("rm -r "+outputdir)
+        os.system("mkdir -p "+outputdir)
+        os.system("mkdir -p "+outputdir+"/log/")
+        os.system("mkdir -p "+outputdir+"/input/")
+        os.system("mkdir -p "+outputdir+"/output/")
+        os.system("mkdir -p "+outputdir+"/src/")
+    
+    
+        input = open(inputlist)
+        inputfiles = input.readlines()
+        ijob=0
+    
+        while (len(inputfiles) > 0):
+            inputfilename = outputdir+"/input/input_"+str(ijob)
+            inputfile = open(inputfilename,'w')
+            for line in range(min(1,len(inputfiles))):
+                ntpfile = inputfiles.pop()
+                if ntpfile != '':
+    	            inputfile.write(ntpfile)
+    
+            inputfile.close()
+    
+    
+            if (z < 4):
+                output = dataset_name[z]+"_"+deltaMRegion[y]
+            else:
+                output = dataset_name[z]
             outputname = outputdir+"/src/"+output+".src"
             outputfile = open(outputname,'w')
             outputfile.write('#!/bin/bash\n')
             outputfile.write('export SCRAM_ARCH=slc6_amd64_gcc472\n')
             outputfile.write('cd '+pwd+'; eval `scramv1 runtime -sh`; source setup.sh; \n')
-	    outputfile.write("./"+executable+" "+ntpfile[:-1]+ " "+outputdir+"/output/"+output+".root "+deltaMArguments[y]+";")
-            outputfile.close
-            os.system("echo bsub -q 1nd -o "+outputdir+"/log/"+output+".log source "+outputname)
-            os.system("bsub -q 1nd -o "+outputdir+"/log/"+output+".log source "+outputname)
-            ijob = ijob+1
-                                                                                                                                                                                        
-            continue
+            if (z < 4):
+                outputfile.write("./"+executable+" "+ntpfile[:-1]+ " "+outputdir+"/output/"+output+".root "+deltaMArguments[y]+";")
+            else:
+                outputfile.write("./"+executable+" "+ntpfile[:-1]+ " "+outputdir+"/output/"+output+".root ;")
+        outputfile.close
+        os.system("echo bsub -q 1nd -o "+outputdir+"/log/"+output+".log source "+outputname)
+        os.system("bsub -q 1nd -o "+outputdir+"/log/"+output+".log source "+outputname)
+        ijob = ijob+1
+                                                                                                                                                                                    
+        continue
