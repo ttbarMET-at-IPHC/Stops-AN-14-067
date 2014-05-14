125e26cfc72a6b34ac7dadc42e8c211df420b526
Alexandre Aubin <alexandre.aubin@cern.ch>
2014-05-15 00:31:48 +0200

    Migrated event/sampleName/sampleType to global variables, removed the CRx names, added trigger requirements for data in channel definitions and temp fix for eta(muon) cut

--- selectionDefinitions.h
+++ selectionDefinitions.h
@@ -1,20 +1,26 @@
-#define MT_CUT 100 
-#define MET_CUT 80
-#define LEPTON_PT_CUT 26  
-#define NJET_CUT 4
+#define MT_CUT    100 
+#define MET_CUT   80
+#define NJET_CUT  4
 #define NBJET_CUT 1
-#define NLEP_CUT 1
+#define NLEP_CUT  1
 
 
 #include "Reader_final0210.h"
-babyEvent myEvent;
 
+// NB : When you call any of the following functions,
+// these three variables must be filled with the current
+// event being taken care of, the name of the sample and
+// the sample type (background, signal or data)
+
+babyEvent myEvent;
+string sampleName;
+string sampleType;
 
 // Control region definitions
-// // ##################################
+// ##########################
 
 
-bool goesInPreselectionCR0() 
+bool goesInPreselection() 
 {
 
     if (myEvent.MET < MET_CUT) return false;
@@ -30,7 +36,7 @@ bool goesInPreselectionCR0()
 
 
 
-bool goesIn0BtagCR1()
+bool goesIn0BtagControlRegion()
 {
 
     if (myEvent.MET < MET_CUT) return false;
@@ -45,7 +51,7 @@ bool goesIn0BtagCR1()
 
 
 
-bool goesInMTPeak()   
+bool goesInMTPeakControlRegion()   
 {
 
     if (myEvent.MET < MET_CUT) return false;
@@ -61,7 +67,7 @@ bool goesInMTPeak()
 
 
 
-bool goesInDileptonCR4() 
+bool goesInDileptonControlRegion() 
 {
 
     // Apply double-lepton trigger requirement (for both MC and data)
@@ -80,7 +86,7 @@ bool goesInDileptonCR4()
 
 
 
-bool goesInDileptonCR5() 
+bool goesInVetosControlRegion() 
 {
 
     // Apply double-lepton trigger requirement (for both MC and data)
@@ -106,33 +112,47 @@ bool goesInDileptonCR5()
 // Single-lepton channels definitions
 // ##################################
 
-bool goesInSingleElecChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInSingleElecChannel() 
 {
     // Keep only events with numberOfLepton == 1
-    if (event.numberOfLepton != NLEP_CUT) return false;
-    // For data, keep only events from SingleElec dataset
-    if ((sampleType == "data") && (sampleName != "SingleElec")) return false;
+    if (myEvent.numberOfLepton != NLEP_CUT) return false;
+    // For data, keep only events from SingleElec dataset that fired the trigger
+    if (sampleType == "data")
+    {
+        if ((sampleName != "SingleElec") || (!myEvent.triggerElec)) return false;
+    }
     // Keep only events with an electron as leading lepton
-    return (abs(event.leadingLeptonPDGId) == 11); 
+    return (abs(myEvent.leadingLeptonPDGId) == 11); 
 }
 
 
-bool goesInSingleMuonChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInSingleMuonChannel() 
 { 
     // Keep only events with numberOfLepton == 1
-    if (event.numberOfLepton != 1) return false;
-    // For data, keep only events from SingleMuon dataset
-    if ((sampleType == "data") && (sampleName != "SingleMuon")) return false;
+    if (myEvent.numberOfLepton != 1) return false;
+    // For data, keep only events from SingleMuon dataset that fired the trigger
+    if (sampleType == "data")
+    {
+        if ((sampleName != "SingleMuon") || ((!myEvent.triggerMuon) && (!myEvent.xtriggerMuon))) return false;
+    }    
+    
+    // Take care of the splitting due to x-trigger
+    if ((myEvent.leadingLepton.Pt() >= 26) && (!myEvent.triggerMuon))  return false;
+    if ((myEvent.leadingLepton.Pt() <  26) && (!myEvent.xtriggerMuon)) return false;
+   
+    // TODO : remove this temporary fix for the eta(muon) < -2.1 after its propagated in the babyTuples
+    if (myEvent.leadingLepton.Eta() < -2.1) return false;
+
     // Keep only events with a muon as leading lepton
-    return (abs(event.leadingLeptonPDGId) == 13); 
+    return (abs(myEvent.leadingLeptonPDGId) == 13); 
 }
 
 
-bool goesInSingleLeptonChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInSingleLeptonChannel() 
 { 
-    // Single-lepton channel is the union of e-channel + µ-channel
-    bool test = (goesInSingleElecChannel(event,sampleName,sampleType) 
-              || goesInSingleMuonChannel(event,sampleName,sampleType));
+    // Single-lepton channel is the union of e-channel + mu-channel
+    bool test = (goesInSingleElecChannel() 
+              || goesInSingleMuonChannel());
     return test;
 }
 
@@ -141,53 +161,62 @@ bool goesInSingleLeptonChannel(babyEvent event, string sampleName, string sample
 // Double-lepton channels definitions
 // ##################################
 
-bool goesInDoubleElecChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInDoubleElecChannel() 
 { 
     // Keep only events with numberOfLepton == 2
-    if (event.numberOfLepton != 2) return false;
-    // For data, keep only events from DoubleElec dataset
-    if ((sampleType == "data") && (sampleName != "DoubleElec")) return false;
+    if (myEvent.numberOfLepton != 2) return false;
+    // For data, keep only events from DoubleElec dataset that fired the trigger
+    if (sampleType == "data")
+    {
+        if ((sampleName != "DoubleElec") || (!myEvent.triggerDoubleElec)) return false;
+    }
     // Keep only events with two electrons
-    return ((abs(event.leadingLeptonPDGId) == 11) 
-         && (abs(event.secondLeptonPDGId)  == 11)); 
+    return ((abs(myEvent.leadingLeptonPDGId) == 11) 
+         && (abs(myEvent.secondLeptonPDGId)  == 11)); 
 }
 
 
 
-bool goesInDoubleMuonChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInDoubleMuonChannel() 
 { 
     // Keep only events with numberOfLepton == 2
-    if (event.numberOfLepton != 2) return false;
-    // For data, keep only events from DoubleMuon dataset
-    if ((sampleType == "data") && (sampleName != "DoubleMuon")) return false;
+    if (myEvent.numberOfLepton != 2) return false;
+    // For data, keep only events from DoubleMuon dataset that fired the trigger
+    if (sampleType == "data")
+    {
+        if ((sampleName != "DoubleMuon") || (!myEvent.triggerDoubleMuon)) return false;
+    }
     // Keep only events with two muons
-    return ((abs(event.leadingLeptonPDGId) == 13) 
-         && (abs(event.secondLeptonPDGId)  == 13)); 
+    return ((abs(myEvent.leadingLeptonPDGId) == 13) 
+         && (abs(myEvent.secondLeptonPDGId)  == 13)); 
 }
 
 
 
-bool goesInMuonElecChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInMuonElecChannel() 
 { 
     // Keep only events with numberOfLepton == 2
-    if (event.numberOfLepton != 2) return false;
-    // For data, keep only events from SingleMuon channel
-    if ((sampleType == "data") && (sampleName != "MuEl")) return false;
+    if (myEvent.numberOfLepton != 2) return false;
+    // For data, keep only events from SingleMuon channel that fired the trigger
+    if (sampleType == "data")
+    {
+        if ((sampleName != "MuEl") || (!myEvent.triggerMuonElec)) return false;
+    }
     // Keep only events with an electron and a muon
-    return   (((abs(event.leadingLeptonPDGId) == 13) 
-            && (abs(event.secondLeptonPDGId)  == 11))
-       ||     ((abs(event.leadingLeptonPDGId) == 11) 
-            && (abs(event.secondLeptonPDGId)  == 13)));
+    return   (((abs(myEvent.leadingLeptonPDGId) == 13) 
+            && (abs(myEvent.secondLeptonPDGId)  == 11))
+       ||     ((abs(myEvent.leadingLeptonPDGId) == 11) 
+            && (abs(myEvent.secondLeptonPDGId)  == 13)));
 }
 
 
 
-bool goesInDoubleLeptonChannel(babyEvent event, string sampleName, string sampleType) 
+bool goesInDoubleLeptonChannel() 
 { 
     // Double-lepton channel is the union of ee, mumu and emu channels
-    bool test = (goesInDoubleElecChannel(event,sampleName,sampleType) 
-              || goesInDoubleMuonChannel(event,sampleName,sampleType)
-              ||   goesInMuonElecChannel(event,sampleName,sampleType)  ); 
+    bool test = (goesInDoubleElecChannel() 
+              || goesInDoubleMuonChannel()
+              ||   goesInMuonElecChannel()  ); 
     return test;
 }
 
@@ -196,43 +225,44 @@ bool goesInDoubleLeptonChannel(babyEvent event, string sampleName, string sample
 // Weighting information
 // #####################
 
-float getLumi(babyEvent event, string sampleName, string sampleType)
+float getLumi()
 {
-         if (goesInSingleElecChannel(event, sampleName, sampleType))  return 19154.0;
-    else if (goesInSingleMuonChannel(event, sampleName, sampleType))  return 19096.0;
-    else if (goesInMuonElecChannel(  event, sampleName, sampleType))  return 19347.0;
-    else if (goesInDoubleMuonChannel(event, sampleName, sampleType))  return 14595.0;
-    else if (goesInDoubleElecChannel(event, sampleName, sampleType))  return 19216.0;
-    else                                                              return 0.0;
+    // TODO : upload these numbers after missing lumi is recovered
+         if (goesInSingleElecChannel())  return 19154.0;
+    else if (goesInSingleMuonChannel())  return 19096.0;
+    else if (goesInMuonElecChannel  ())  return 19347.0;
+    else if (goesInDoubleMuonChannel())  return 14595.0;
+    else if (goesInDoubleElecChannel())  return 19216.0;
+    else                                                       return 0.0;
 }
 
-float getWeight(babyEvent event, string sampleName, string sampleType)
+float getWeight()
 {
     float weight = 1.0;
     if (sampleType == "data") return weight;
 
     // Get the lumi
-    float lumi = getLumi(event,sampleName,sampleType);
+    float lumi = getLumi();
 
     // Normalize to cross section times lumi
-    weight *= event.weightCrossSection * lumi;
+    weight *= myEvent.weightCrossSection * lumi;
 
     // Apply trigger efficiency weights for singleLepton channels
-    // TODO : Add number for dilepton 
-    if (event.numberOfLepton == 1)
-        weight *= event.weightTriggerEfficiency;
+    // TODO : Add trigger efficiency for dilepton in babyTuple
+    if (myEvent.numberOfLepton == 1)
+        weight *= myEvent.weightTriggerEfficiency;
 
     // Apply pile-up weight 
     // TODO : Do we confirm we'll use also this for signal ?
-    weight *= event.weightPileUp;
+    weight *= myEvent.weightPileUp;
 
     // For signal, apply ISR reweighting
     if (sampleType == "signal")  
-        weight *= event.weightISRmodeling;
+        weight *= myEvent.weightISRmodeling;
 
     // For ttbar only, apply topPt reweighting
     if (sampleName.find("ttbar") != string::npos) 
-        weight *= event.weightTopPt;
+        weight *= myEvent.weightTopPt;
 
     return weight;
 }
