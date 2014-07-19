0a979b6dbeeda0b144508a0ade98cad17e27a2d2
Administrator <sigamani@cern.ch>
2014-07-19 18:53:34 +0200

    adding latest bkg predictions from Alex

---
+++ backgroundPredictions_LATEST_MT100.h
@@ -0,0 +1,37 @@
+pair<float,float> backgroundPredition_BDT_T2bw025_1                     (float BDTcutOffset = 0.0) { return pair<float,float>(125.9352  , 21.7516  ); }
+pair<float,float> backgroundPredition_BDT_T2bw025_3                     (float BDTcutOffset = 0.0) { return pair<float,float>(84.8036   , 18.9210  ); }
+pair<float,float> backgroundPredition_BDT_T2bw025_4                     (float BDTcutOffset = 0.0) { return pair<float,float>(19.4851   , 5.6721   ); }
+pair<float,float> backgroundPredition_BDT_T2bw025_6                     (float BDTcutOffset = 0.0) { return pair<float,float>(5.4788    , 2.1535   ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_1_highDM              (float BDTcutOffset = 0.0) { return pair<float,float>(22.0163   , 7.3508   ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_1_lowDM               (float BDTcutOffset = 0.0) { return pair<float,float>(89.1315   , 25.0006  ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_3                     (float BDTcutOffset = 0.0) { return pair<float,float>(6.9297    , 2.5978   ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_4                     (float BDTcutOffset = 0.0) { return pair<float,float>(0.7766    , 0.3199   ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_5                     (float BDTcutOffset = 0.0) { return pair<float,float>(3.2600    , 1.4785   ); }
+pair<float,float> backgroundPredition_BDT_T2bw050_6                     (float BDTcutOffset = 0.0) { return pair<float,float>(2.7691    , 1.4161   ); }
+pair<float,float> backgroundPredition_BDT_T2bw075_1                     (float BDTcutOffset = 0.0) { return pair<float,float>(12.9658   , 4.3121   ); }
+pair<float,float> backgroundPredition_BDT_T2bw075_2                     (float BDTcutOffset = 0.0) { return pair<float,float>(22.9897   , 8.0637   ); }
+pair<float,float> backgroundPredition_BDT_T2bw075_3                     (float BDTcutOffset = 0.0) { return pair<float,float>(5.2788    , 2.2471   ); }
+pair<float,float> backgroundPredition_BDT_T2bw075_5_highDM              (float BDTcutOffset = 0.0) { return pair<float,float>(0.3632    , 0.2140   ); }
+pair<float,float> backgroundPredition_BDT_T2bw075_5_lowDM               (float BDTcutOffset = 0.0) { return pair<float,float>(2.2925    , 1.1515   ); }
+pair<float,float> backgroundPredition_BDT_T2tt_1                        (float BDTcutOffset = 0.0) { return pair<float,float>(45.7587   , 17.1473  ); }
+pair<float,float> backgroundPredition_BDT_T2tt_2_highDM                 (float BDTcutOffset = 0.0) { return pair<float,float>(192.6494  , 52.7748  ); }
+pair<float,float> backgroundPredition_BDT_T2tt_2_lowDM                  (float BDTcutOffset = 0.0) { return pair<float,float>(36.8139   , 11.2915  ); }
+pair<float,float> backgroundPredition_BDT_T2tt_5_highDM                 (float BDTcutOffset = 0.0) { return pair<float,float>(1.9100    , 1.1695   ); }
+pair<float,float> backgroundPredition_BDT_T2tt_5_lowDM                  (float BDTcutOffset = 0.0) { return pair<float,float>(2.2924    , 1.2899   ); }
+pair<float,float> backgroundPredition_BDT_T2tt_5_mediumDM               (float BDTcutOffset = 0.0) { return pair<float,float>(3.5712    , 2.0451   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw025_highMasses    ()                         { return pair<float,float>(1.8211    , 0.9624   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw025_lowMasses     ()                         { return pair<float,float>(11.9444   , 3.6439   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw025_offShell      ()                         { return pair<float,float>(24.1043   , 8.8006   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw050_highDeltaM    ()                         { return pair<float,float>(2.5088    , 1.1098   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw050_lowMasses     ()                         { return pair<float,float>(96.5390   , 15.6150  ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw050_mediumDeltaM  ()                         { return pair<float,float>(29.2855   , 7.9275   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw050_offShell      ()                         { return pair<float,float>(24.1043   , 8.8006   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw075_highDeltaM    ()                         { return pair<float,float>(4.7748    , 1.8662   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw075_lowDeltaM     ()                         { return pair<float,float>(5.2541    , 2.2091   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2bw075_mediumDeltaM  ()                         { return pair<float,float>(34.5702   , 8.9969   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2tt_highDeltaM       ()                         { return pair<float,float>(3.4188    , 1.9632   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2tt_lowDeltaM        ()                         { return pair<float,float>(171.6268  , 19.4856  ); }
+pair<float,float> backgroundPredition_cutAndCount_T2tt_mediumDeltaM     ()                         { return pair<float,float>(10.3495   , 2.6380   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2tt_offShellLoose    ()                         { return pair<float,float>(38.2671   , 8.5935   ); }
+pair<float,float> backgroundPredition_cutAndCount_T2tt_offShellTight    ()                         { return pair<float,float>(11.5363   , 6.1709   ); }
+pair<float,float> backgroundPredition_preselection                      ()                         { return pair<float,float>(11644.0518, 1819.3839); }
