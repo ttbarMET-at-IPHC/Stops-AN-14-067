1f0985bbd4393435500ce614c201ff63f584f18c
Alexandre Aubin <alexandre.aubin@cern.ch>
2014-07-29 19:17:42 +0200

    Propagating the effect of the Z veto fix to the background prediction

--- backgroundPredictions_MT100.h
+++ backgroundPredictions_MT100.h
@@ -1,38 +1,39 @@
 pair<double,double> backgroundPrediction_BDT_T2bw025_1                   (double BDTcutOffset = 0.0) { return pair<double,double>(129.5715,   26.3946  ); }
-pair<double,double> backgroundPrediction_BDT_T2bw025_3_lowDM             (double BDTcutOffset = 0.0) { return pair<double,double>(88.4375,    24.3092  ); }
 pair<double,double> backgroundPrediction_BDT_T2bw025_3_highDM            (double BDTcutOffset = 0.0) { return pair<double,double>(3.1555,     1.6466   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw025_3_lowDM             (double BDTcutOffset = 0.0) { return pair<double,double>(88.4375,    24.3092  ); }
 pair<double,double> backgroundPrediction_BDT_T2bw025_4                   (double BDTcutOffset = 0.0) { return pair<double,double>(19.9847,    6.2634   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw025_6                   (double BDTcutOffset = 0.0) { return pair<double,double>(5.8498,     2.5691   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw050_1_highDM            (double BDTcutOffset = 0.0) { return pair<double,double>(22.5160,    7.7035   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw050_1_lowDM             (double BDTcutOffset = 0.0) { return pair<double,double>(91.2816,    26.8538  ); }
-pair<double,double> backgroundPrediction_BDT_T2bw050_3                   (double BDTcutOffset = 0.0) { return pair<double,double>(7.0636,     2.6976   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw025_6                   (double BDTcutOffset = 0.0) { return pair<double,double>(5.8498,     2.5183   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw050_1_highDM            (double BDTcutOffset = 0.0) { return pair<double,double>(22.5160,    7.6761   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw050_1_lowDM             (double BDTcutOffset = 0.0) { return pair<double,double>(91.2816,    26.7520  ); }
+pair<double,double> backgroundPrediction_BDT_T2bw050_3                   (double BDTcutOffset = 0.0) { return pair<double,double>(7.0636,     2.4887   ); }
 pair<double,double> backgroundPrediction_BDT_T2bw050_4                   (double BDTcutOffset = 0.0) { return pair<double,double>(0.7824,     0.3205   ); }
 pair<double,double> backgroundPrediction_BDT_T2bw050_5                   (double BDTcutOffset = 0.0) { return pair<double,double>(3.3373,     1.5419   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw050_6                   (double BDTcutOffset = 0.0) { return pair<double,double>(2.7323,     1.3784   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw075_1                   (double BDTcutOffset = 0.0) { return pair<double,double>(13.2689,    4.5338   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw050_6                   (double BDTcutOffset = 0.0) { return pair<double,double>(2.7323,     1.3394   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw075_1                   (double BDTcutOffset = 0.0) { return pair<double,double>(13.2689,    4.0458   ); }
 pair<double,double> backgroundPrediction_BDT_T2bw075_2                   (double BDTcutOffset = 0.0) { return pair<double,double>(23.1362,    8.1749   ); }
-pair<double,double> backgroundPrediction_BDT_T2bw075_3                   (double BDTcutOffset = 0.0) { return pair<double,double>(5.2028,     2.1651   ); }
+pair<double,double> backgroundPrediction_BDT_T2bw075_3                   (double BDTcutOffset = 0.0) { return pair<double,double>(5.2028,     2.0296   ); }
 pair<double,double> backgroundPrediction_BDT_T2bw075_5_highDM            (double BDTcutOffset = 0.0) { return pair<double,double>(0.3632,     0.2140   ); }
 pair<double,double> backgroundPrediction_BDT_T2bw075_5_lowDM             (double BDTcutOffset = 0.0) { return pair<double,double>(2.2578,     1.1356   ); }
 pair<double,double> backgroundPrediction_BDT_T2tt_1                      (double BDTcutOffset = 0.0) { return pair<double,double>(101.0314,   29.4811  ); }
-pair<double,double> backgroundPrediction_BDT_T2tt_2_highDM               (double BDTcutOffset = 0.0) { return pair<double,double>(16.0594,    6.4093   ); }
-pair<double,double> backgroundPrediction_BDT_T2tt_2_lowDM                (double BDTcutOffset = 0.0) { return pair<double,double>(37.1207,    16.9635  ); }
+pair<double,double> backgroundPrediction_BDT_T2tt_2_highDM               (double BDTcutOffset = 0.0) { return pair<double,double>(16.0594,    5.0642   ); }
+pair<double,double> backgroundPrediction_BDT_T2tt_2_lowDM                (double BDTcutOffset = 0.0) { return pair<double,double>(37.1207,    11.3233  ); }
 pair<double,double> backgroundPrediction_BDT_T2tt_5_highDM               (double BDTcutOffset = 0.0) { return pair<double,double>(1.9454,     1.2176   ); }
 pair<double,double> backgroundPrediction_BDT_T2tt_5_lowDM                (double BDTcutOffset = 0.0) { return pair<double,double>(2.3211,     1.3199   ); }
 pair<double,double> backgroundPrediction_BDT_T2tt_5_mediumDM             (double BDTcutOffset = 0.0) { return pair<double,double>(3.6069,     2.0745   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw025_highMasses  () { return pair<double,double>(1.8211,     0.9624   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw025_lowMasses   () { return pair<double,double>(12.0031,    3.7013   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw025_offShell    () { return pair<double,double>(24.1765,    8.8983   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw050_highDeltaM  () { return pair<double,double>(2.5166,     1.1324   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw050_lowMasses   () { return pair<double,double>(97.0783,    16.2797  ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw050_mediumDeltaM() { return pair<double,double>(31.0122,    8.7599   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw050_offShell    () { return pair<double,double>(24.1765,    8.8983   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw075_highDeltaM  () { return pair<double,double>(5.1450,     2.0184   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw025_highMasses  () { return pair<double,double>(1.8211,     0.9338   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw025_lowMasses   () { return pair<double,double>(12.0031,    4.2772   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw025_offShell    () { return pair<double,double>(24.1765,    7.3290   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw050_highDeltaM  () { return pair<double,double>(2.5166,     1.1039   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw050_lowMasses   () { return pair<double,double>(97.0783,    14.3624  ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw050_mediumDeltaM() { return pair<double,double>(31.0122,    8.6789   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw050_offShell    () { return pair<double,double>(24.1765,    7.3290   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw075_highDeltaM  () { return pair<double,double>(5.1450,     2.0179   ); }
 pair<double,double> backgroundPrediction_cutAndCount_T2bw075_lowDeltaM   () { return pair<double,double>(5.2540,     2.2087   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2bw075_mediumDeltaM() { return pair<double,double>(34.0299,    7.9540   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2tt_highDeltaM     () { return pair<double,double>(3.4188,     1.9632   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2tt_lowDeltaM      () { return pair<double,double>(178.8980,   22.2654  ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2bw075_mediumDeltaM() { return pair<double,double>(34.0299,    6.9528   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2tt_highDeltaM     () { return pair<double,double>(3.4188,     1.4003   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2tt_lowDeltaM      () { return pair<double,double>(178.8980,   21.3484  ); }
 pair<double,double> backgroundPrediction_cutAndCount_T2tt_mediumDeltaM   () { return pair<double,double>(10.6587,    2.5934   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2tt_offShellLoose  () { return pair<double,double>(38.5279,    8.6621   ); }
-pair<double,double> backgroundPrediction_cutAndCount_T2tt_offShellTight  () { return pair<double,double>(11.3975,    6.0908   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2tt_offShellLoose  () { return pair<double,double>(38.5279,    8.3234   ); }
+pair<double,double> backgroundPrediction_cutAndCount_T2tt_offShellTight  () { return pair<double,double>(11.3975,    4.3959   ); }
 pair<double,double> backgroundPrediction_preselection                    () { return pair<double,double>(9940.2217,  1664.9653); }
+
